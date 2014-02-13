e9d6399b518833552f570981326ae20617eba7e9
Dima Krasner <dima@dimakrasner.com>
2014-02-13 19:46:41 +0200

    Converted all spaces to tabs.

--- tinyunmute.c
+++ tinyunmute.c
@@ -9,18 +9,18 @@ int main() {
 	int exit_code = EXIT_FAILURE;
 
 	/* the mixer */
-    struct mixer *mixer;
+	struct mixer *mixer;
 
 	/* the number of mixer controls */
-    unsigned int controls_count;
+	unsigned int controls_count;
 
-    /* a mixer control */
-    struct mixer_ctl *control;
+	/* a mixer control */
+	struct mixer_ctl *control;
 
-    /* the control type */
-    enum mixer_ctl_type type;
+	/* the control type */
+	enum mixer_ctl_type type;
 
-    /* the number of control values */
+	/* the number of control values */
 	unsigned int values_count;
 
 	/* loop counters */
@@ -28,21 +28,21 @@ int main() {
 	unsigned int j;
 
 	/* open the mixer */
-    mixer = mixer_open(CARD_ID);
-    if (NULL == mixer)
+	mixer = mixer_open(CARD_ID);
+	if (NULL == mixer)
 		goto end;
 
 	/* get the number of controls */
-    controls_count = mixer_get_num_ctls(mixer);
+	controls_count = mixer_get_num_ctls(mixer);
 
-    for (i = 0; controls_count > i; ++i) {
+	for (i = 0; controls_count > i; ++i) {
 		/* read the control information */
-        control = mixer_get_ctl(mixer, i);
+		control = mixer_get_ctl(mixer, i);
 		type = mixer_ctl_get_type(control);
-        values_count = mixer_ctl_get_num_values(control);
+		values_count = mixer_ctl_get_num_values(control);
 
-        /* ignore non-boolean controls */
-        if (MIXER_CTL_TYPE_BOOL != type)
+		/* ignore non-boolean controls */
+		if (MIXER_CTL_TYPE_BOOL != type)
 			continue;
 
 		/* unmute the control */
@@ -50,7 +50,7 @@ int main() {
 			if (0 != mixer_ctl_set_value(control, j, 1))
 				goto close_mixer;
 		}
-    }
+	}
 
 	/* report success */
 	exit_code = EXIT_SUCCESS;
