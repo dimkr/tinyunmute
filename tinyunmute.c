d0bbc466ac0c1861abfe3cfdd8107927443163d7
Dima Krasner <dima@dimakrasner.com>
2014-09-23 21:42:40 +0300

    Bug fix - we should go on if unmuting a single control failed.

--- tinyunmute.c
+++ tinyunmute.c
@@ -8,41 +8,41 @@ int main() {
 	/* the exit code */
 	int exit_code = EXIT_FAILURE;
 
-	/* the mixer */
-	struct mixer *mixer;
-
 	/* the number of mixer controls */
-	unsigned int controls_count;
-
-	/* a mixer control */
-	struct mixer_ctl *control;
+	unsigned int controls_count = 0;
 
 	/* the control type */
-	enum mixer_ctl_type type;
+	enum mixer_ctl_type type = MIXER_CTL_TYPE_BOOL;
 
 	/* the number of control values */
-	unsigned int values_count;
+	unsigned int values_count = 0;
 
 	/* a control value */
-	int value;
+	int value = 0;
 
 	/* loop counters */
-	unsigned int i;
-	unsigned int j;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	/* a mixer control */
+	struct mixer_ctl *control = NULL;
+
+	/* the mixer */
+	struct mixer *mixer = NULL;
 
 	/* open the mixer */
 	mixer = mixer_open(CARD_ID);
-	if (NULL == mixer)
+	if (NULL == mixer) {
 		goto end;
+	}
 
 	/* get the number of controls */
 	controls_count = mixer_get_num_ctls(mixer);
 
-	for (i = 0; controls_count > i; ++i) {
+	for ( ; controls_count > i; ++i) {
 		/* read the control information */
 		control = mixer_get_ctl(mixer, i);
 		type = mixer_ctl_get_type(control);
-		values_count = mixer_ctl_get_num_values(control);
 
 		/* decide which value to assign */
 		switch (type) {
@@ -56,20 +56,17 @@ int main() {
 
 			default:
 				continue;
-
 		}
 
 		/* unmute the control */
+		values_count = mixer_ctl_get_num_values(control);
 		for (j = 0; values_count > j; ++j) {
-			if (0 != mixer_ctl_set_value(control, j, value))
-				goto close_mixer;
+			if (0 == mixer_ctl_set_value(control, j, value)) {
+				exit_code = EXIT_SUCCESS;
+			}
 		}
 	}
 
-	/* report success */
-	exit_code = EXIT_SUCCESS;
-
-close_mixer:
 	/* close the mixer */
 	mixer_close(mixer);
 
